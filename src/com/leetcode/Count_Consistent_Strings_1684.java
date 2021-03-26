c610fef187a44ed1f5b0449b1386e109e1eb4b7f
UMESHvaidya <vaidyaumesh36@gmail.com>
2021-03-26 08:56:42 +0530

    Count the Number of Consistent Strings 1684 optimized

--- src/com/leetcode/Count_Consistent_Strings_1684.java
+++ src/com/leetcode/Count_Consistent_Strings_1684.java
@@ -1,36 +1,26 @@
 package com.leetcode;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static java.lang.System.*;
 
 public class Count_Consistent_Strings_1684 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String allowed = "abc";
         String[]words = {"a","b","c","ab","ac","bc","abcd"};
-        System.out.println(countConsistentStrings(allowed,words));
+        out.println(countConsistentStrings(allowed,words));
     }
     public static int countConsistentStrings(String allowed, String[] words) {
-        int ans = 0;
-        Integer arr[] = new Integer[allowed.length()];
-        for (int i = 0; i < arr.length; i++)
-                arr[i] = (int)allowed.charAt(i);
-        Set<Integer> mySet = new HashSet<>();
-        Collections.addAll(mySet,arr);
-        for (int i = 0; i < words.length; i++) {
-            Integer brr[] = new Integer[words[i].length()];
-            for (int j = 0; j < brr.length; j++) {
-                brr[j] = (int) words[i].charAt(j);
+        int res = words.length;
+        int k[] = new int[123];
+        for(int i =0;i<allowed.length();i++)
+            k[(int)allowed.charAt(i)]=1;
+        for (String s : words) {
+            if (IntStream.range(0, s.length()).anyMatch(j -> k[(int) s.charAt(j)] != 1)) {
+                res--;
             }
-            Set<Integer> mySet_2 = new HashSet<>();
-            Collections.addAll(mySet_2,brr);
-            Set<Integer> result = new HashSet<>(mySet);
-            result.addAll(mySet_2);
-            if(mySet.equals(result)) ans+= 1;
-
         }
-        return ans;
+        return res;
     }
 }
