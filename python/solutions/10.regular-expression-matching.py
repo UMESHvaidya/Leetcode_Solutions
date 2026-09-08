f30f01fb4fde95e81d5c2ce07760211947eae26e
Umesh Vaidya <umesh@Umeshs-MacBook-Air.local>
2026-09-09 00:18:34 +0530

    Add LeetCode 10: Regular Expression Matching solution

---
+++ python/solutions/10.regular-expression-matching.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if j == len(p):
+                return i == len(s)
+            
+            first_match = i < len(s) and p[j] in {s[i], '.'}
+
+            if j + 1 < len(p) and p[j+1] == '*':
+                # Case 1: * matches zero of the preceding element
+                # Case 2: * matches one or more of the preceding element
+                res = dp(i, j + 2) or (first_match and dp(i + 1, j))
+            else:
+                res = first_match and dp(i + 1, j + 1)
+            
+            memo[(i, j)] = res
+            return res
+
+        return dp(0, 0)
